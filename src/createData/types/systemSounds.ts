8ddddd7b50e93df9e13cb9b62fd6379de2c422b2
Sigureya <sigureya1208@gmail.com>
2025-01-15 08:10:16 +0900

    systemSoundsの名前付きバージョン

---
+++ src/createData/types/systemSounds.ts
@@ -0,0 +1,28 @@
+import type { AudioFileParams } from "@sigureya/rpgtypes";
+
+export interface SystemSounds {
+  cursor: AudioFileParams;
+  ok: AudioFileParams;
+  cancel: AudioFileParams;
+  buzzer: AudioFileParams;
+  equip: AudioFileParams;
+  save: AudioFileParams;
+  load: AudioFileParams;
+  battleStart: AudioFileParams;
+  escape: AudioFileParams;
+  enemyAttack: AudioFileParams;
+  enemyDamage: AudioFileParams;
+  enemyCollapse: AudioFileParams;
+  bossCollapes1: AudioFileParams;
+  bossCollapes2: AudioFileParams;
+  actorDamage: AudioFileParams;
+  actorCollapse: AudioFileParams;
+  playRecovery: AudioFileParams;
+  playMiss: AudioFileParams;
+  playEvasion: AudioFileParams;
+  playMagicEvasion: AudioFileParams;
+  playReflection: AudioFileParams;
+  shop: AudioFileParams;
+  useItem: AudioFileParams;
+  useSkill: AudioFileParams;
+}
