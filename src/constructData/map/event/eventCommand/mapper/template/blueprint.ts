c6fb19449eafefd26a033f9be1a00835294d184a
Sigureya <sigureya1208@gmail.com>
2025-01-18 04:13:58 +0900

    クラスを設計図とファクトリに分割。

---
+++ src/constructData/map/event/eventCommand/mapper/template/blueprint.ts
@@ -0,0 +1,43 @@
+// 設計図
+export interface Blueprint<
+  ParameterArray extends unknown[],
+  MappedParamObject extends object
+> {
+  construct(proto: Partial<MappedParamObject>): MappedParamObject;
+  toArray(parameters: MappedParamObject): ParameterArray;
+  fromArray(parameters: ParameterArray): MappedParamObject;
+  validate?(parameters: MappedParamObject): void;
+}
+
+export const createSample = <
+  ParameterArray extends unknown[],
+  MappedParamObject extends object
+>(
+  blueprint: Blueprint<ParameterArray, MappedParamObject>
+) => {
+  const proto = blueprint.construct({});
+  const array = blueprint.toArray(proto);
+  return { object: createTypeMap(proto), array: typeMapFromArray(array) };
+};
+
+export const typeMapFromArray = (
+  array: unknown[]
+): ReadonlyMap<string, string> => {
+  return new Map(array.map((value, index) => [index.toString(), typeof value]));
+};
+
+export const createTypeMap = <K extends string, V extends Record<K, V>>(
+  obj: Record<K, V>
+): ReadonlyMap<string, string> => {
+  const list = Object.entries(obj).map(
+    ([key, value]) => [key as K, typeof value] as const
+  );
+  return new Map(list);
+};
+
+export interface TypeInfo {
+  key: string;
+  requiredType: string | undefined;
+  type: string;
+  value: unknown;
+}
--- src/constructData/map/event/eventCommand/mapper/template/parameterFactory.ts
+++ src/constructData/map/event/eventCommand/mapper/template/parameterFactory.ts
@@ -1,11 +1,48 @@
-export interface ParameterFactory<
+import type { TypeInfo, Blueprint } from "./blueprint";
+import { createTypeMap } from "./blueprint";
+
+class Factory<
   ParameterArray extends unknown[],
   MappedParamObject extends object
 > {
-  constructParamArray(proto?: Partial<MappedParamObject>): ParameterArray;
-  constructMappedParamObject(
-    proto?: Partial<MappedParamObject>
-  ): MappedParamObject;
-  toArray(parameters: MappedParamObject): ParameterArray;
-  fromArray(parameters: ParameterArray): MappedParamObject;
+  private _typeMap = createTypeMap(this._blueprint.construct({}));
+  constructor(
+    private readonly _blueprint: Blueprint<ParameterArray, MappedParamObject>
+  ) {}
+
+  validate(parameters: Partial<MappedParamObject>) {
+    const typeInfos = Object.entries(parameters).map<TypeInfo>(
+      ([key, value]) => ({
+        key,
+        requiredType: this._typeMap.get(key),
+        type: typeof value,
+        value,
+      })
+    );
+
+    if (typeInfos.length !== this._typeMap.size) {
+      throw new Error("length is invalid", { cause: typeInfos });
+    }
+
+    if (typeInfos.some((info) => info.type !== info.requiredType)) {
+      throw new Error("undefined type", { cause: typeInfos });
+    }
+    if (this._blueprint.validate) {
+      this._blueprint.validate(parameters as MappedParamObject);
+    }
+  }
+  array(proto: Partial<MappedParamObject> = {}): ParameterArray {
+    const newParam = this._blueprint.construct(proto);
+    this.validate(newParam);
+    return this._blueprint.toArray(newParam);
+  }
+  construct(proto: Partial<MappedParamObject> = {}): MappedParamObject {
+    const newParam = this._blueprint.construct(proto);
+    this.validate(newParam);
+    return newParam;
+  }
+  fromArray(parameters: ParameterArray): MappedParamObject {
+    const newParam = this._blueprint.fromArray(parameters);
+    return this.construct(newParam);
+  }
 }
