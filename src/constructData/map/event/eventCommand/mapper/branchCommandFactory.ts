20d0ea4a575acb559098d9cfffdca6cbbef7719e
Sigureya <sigureya1208@gmail.com>
2025-01-21 10:56:48 +0900

    条件分岐製造クラス。パラメータごとに個別に用意してある

---
+++ src/constructData/map/event/eventCommand/mapper/branchCommandFactory.ts
@@ -0,0 +1,17 @@
+import type { EventCommandTable } from "@sigureya/rpgtypes";
+import { ParameterFactory } from "./template";
+
+export class BranchCommandFactory<
+  MappedParamObject extends object,
+  ParameterArray extends EventCommandTable["CONDITIONAL_BRANCH"]["parameters"]
+> extends ParameterFactory<MappedParamObject, ParameterArray> {
+  createCommand(
+    arg: Partial<MappedParamObject>
+  ): EventCommandTable["CONDITIONAL_BRANCH"] {
+    return {
+      code: 111,
+      indent: 0,
+      parameters: this.array(arg),
+    };
+  }
+}
