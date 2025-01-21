316672a27ca77b550e6043464a929528253aa532
Sigureya <sigureya1208@gmail.com>
2025-01-21 11:02:25 +0900

    一部のenumっぽいオブジェクトを定義。条件分岐コマンドの生成とかで使ってる

--- src/constructData/map/event/eventCommand/mapper/branch/enumMaps.ts
+++ src/constructData/map/event/eventCommand/mapper/branch/enumMaps.ts
@@ -6,16 +6,25 @@ import type {
 } from "@sigureya/rpgtypes";
 import { TwoWayMap } from "./twoWayMap";
 
-export const TOGGLE = new TwoWayMap<Toggle>({ OFF: 1, ON: 0 });
-export const DESIGNATION = new TwoWayMap<Designation>({
-  DIRECT: 0,
-  VARIABLE: 1,
-});
-export const OPETION_PLUSMINUS = new TwoWayMap<Operation_PlusMinus>({
-  PLUS: 0,
-  MINUS: 1,
-});
-export const OPERATION_ADDSUB = new TwoWayMap<Operation_AddSub>({
-  ADD: 0,
-  SUB: 1,
-});
+export const TOGGLE = new TwoWayMap<Toggle>({ OFF: 1, ON: 0 }, "ON");
+export const DESIGNATION = new TwoWayMap<Designation>(
+  {
+    DIRECT: 0,
+    VARIABLE: 1,
+  },
+  "DIRECT"
+);
+export const OPETION_PLUSMINUS = new TwoWayMap<Operation_PlusMinus>(
+  {
+    PLUS: 0,
+    MINUS: 1,
+  },
+  "PLUS"
+);
+export const OPERATION_ADDSUB = new TwoWayMap<Operation_AddSub>(
+  {
+    ADD: 0,
+    SUB: 1,
+  },
+  "ADD"
+);
