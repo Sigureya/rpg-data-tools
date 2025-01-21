c7c0a4d352b1b7e40cfb22dbfd8e45befae7bc80
Sigureya <sigureya1208@gmail.com>
2025-01-21 10:56:23 +0900

    不要になったので削除

--- src/constructData/map/event/eventCommand/mapper/branchParamFactory2.ts
+++
@@ -1,5 +0,0 @@
-import type { BranchCode } from "@sigureya/rpgtypes";
-import type { ParameterFactory } from "./template";
-
-type BranchParameterFactory2<BranchObject extends { branchCode: BranchCode }> =
-  ParameterFactory<BranchObject, []>;
