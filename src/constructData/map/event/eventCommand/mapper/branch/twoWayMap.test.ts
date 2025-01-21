b24ae8fef6394328b7e1175bba46bb4abac6bebe
Sigureya <sigureya1208@gmail.com>
2025-01-21 11:00:16 +0900

    双方向マップ。undefinedを良い感じに対応できるようにした

--- src/constructData/map/event/eventCommand/mapper/branch/twoWayMap.test.ts
+++ src/constructData/map/event/eventCommand/mapper/branch/twoWayMap.test.ts
@@ -8,16 +8,18 @@ const mockTable: Toggle = {
 };
 
 describe("constructEventCommand", () => {
-  const mockMap = new TwoWayMap(mockTable);
+  const mockMap = new TwoWayMap(mockTable, "ON");
 
   test("getValueFromKey", () => {
-    expect(mockMap.getValueFromKey("ON")).toBe(0);
-    expect(mockMap.getValueFromKey("OFF")).toBe(1);
-    expect(mockMap.getValueFromKey("INVALID")).toBeUndefined();
+    expect(mockMap.getValue("ON")).toBe(0);
+    expect(mockMap.getValue("OFF")).toBe(1);
+    expect(mockMap.getValue("INVALID")).toBe(mockMap.defaultValue);
+    expect(mockMap.getValue(undefined)).toBe(mockMap.defaultValue);
   });
   test("getKeyFromValue", () => {
-    expect(mockMap.getKeyFromValue(0)).toBe("ON");
-    expect(mockMap.getKeyFromValue(1)).toBe("OFF");
-    expect(mockMap.getKeyFromValue(2)).toBeUndefined();
+    expect(mockMap.getKey(0)).toBe("ON");
+    expect(mockMap.getKey(1)).toBe("OFF");
+    expect(mockMap.getKey(2)).toBe(mockMap.defaultKey);
+    expect(mockMap.getKey(undefined)).toBe(mockMap.defaultKey);
   });
 });
