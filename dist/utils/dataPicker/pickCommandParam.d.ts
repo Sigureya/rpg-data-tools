bfe6925c39d80f86cc8a6d0f013d6505212d97e8
Sigureya <sigureya1208@gmail.com>
2025-01-23 17:20:52 +0900

    イベントコマンドのパラメータを引っ張り出す関数

---
+++ dist/utils/dataPicker/pickCommandParam.d.ts
@@ -0,0 +1,20 @@
+import { IndexOfCommandParameter } from '@sigureya/rpgtypes';
+export interface CommandParameter<T> {
+    code: number;
+    paramIndex: number;
+    value: T;
+}
+export declare const pickCommandParamString: <Params extends unknown[], Index extends IndexOfCommandParameter<{
+    code: number;
+    parameters: Params;
+}, string>>(command: {
+    code: number;
+    parameters: Params;
+}, index: Index) => CommandParameter<Params[Index]>;
+export declare const pickCommandParamNumber: <Params extends unknown[], Index extends IndexOfCommandParameter<{
+    code: number;
+    parameters: Params;
+}, number>>(command: {
+    code: number;
+    parameters: Params;
+}, index: Index) => CommandParameter<Params[Index]>;
---
+++ src/utils/dataPicker/pickCommandParam.ts
@@ -0,0 +1,37 @@
+import type { IndexOfCommandParameter } from "@sigureya/rpgtypes";
+
+export interface CommandParameter<T> {
+  code: number;
+  paramIndex: number;
+  value: T;
+}
+
+export const pickCommandParamString = <
+  Params extends unknown[],
+  Index extends IndexOfCommandParameter<
+    { code: number; parameters: Params },
+    string
+  >
+>(
+  command: { code: number; parameters: Params },
+  index: Index
+): CommandParameter<Params[Index]> => ({
+  code: command.code,
+  paramIndex: index,
+  value: command.parameters[index],
+});
+
+export const pickCommandParamNumber = <
+  Params extends unknown[],
+  Index extends IndexOfCommandParameter<
+    { code: number; parameters: Params },
+    number
+  >
+>(
+  command: { code: number; parameters: Params },
+  index: Index
+): CommandParameter<Params[Index]> => ({
+  code: command.code,
+  paramIndex: index,
+  value: command.parameters[index],
+});
