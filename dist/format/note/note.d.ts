c780b7564850ff12b774f6814e44ceb8d1d964e5
Sigureya <sigureya1208@gmail.com>
2025-01-23 10:03:23 +0900

    オプションからコールバックに差し替え。こっちの方が柔軟性が高い

--- dist/format/note/note.d.ts
+++ dist/format/note/note.d.ts
@@ -1,10 +1,11 @@
-import { ReadNoteOption, NoteReadResult } from './types';
+import { NoteReadResult } from './types';
 export declare const createNoteEntity: (key: string, value: string) => string;
 export declare const makeRegex: () => RegExp;
-export declare const readNoteObject: (data: {
+export declare const readNoteObject: <Result, T extends {
     note: string;
-}, options?: Partial<ReadNoteOption>) => NoteReadResult[];
-export declare const readNote: (note: string, options?: Partial<ReadNoteOption>) => NoteReadResult[];
+}>(data: T, fn: (key: string, value: string, data: T) => Result) => Result[];
+export declare const readNote: (note: string) => NoteReadResult[];
+export declare const readNoteEx: <Result>(note: string, fn: (key: string, value: string) => Result) => Result[];
 export declare const replaceNote: (note: string, transformFunction: (key: string, value: string) => string) => string;
 export declare const getNoteValue: (note: string, targetKey: string) => string | undefined;
 export declare const setNoteValue: (note: string, targetKey: string, newValue: string) => string;
