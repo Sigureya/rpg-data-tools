83a04ddcaada347a1dcc161a2b5a9c12ede26871
Sigureya <sigureya1208@gmail.com>
2025-01-24 22:34:31 +0900

    テストコードで使うためにパスを設定

---
+++ src/commandMapper/commandProxy/proxy.test.ts
@@ -0,0 +1,6 @@
+import { describe, test, expect } from "vitest";
+import { comment } from "@src/constructData/";
+
+const commands = [comment.constructe({ content: "aaaa" })];
+
+describe("comment", () => {});
