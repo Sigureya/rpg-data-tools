69e7eaaae6817f893a16079b5d3cfdc89109200c
Sigureya <sigureya1208@gmail.com>
2025-01-28 07:14:16 +0900

    型関連はtypesにまとめる

---
+++ src/commandMapper/commandProxy/types.ts
@@ -0,0 +1,18 @@
+export interface commandProxy3 {
+  join(sepadrator?: string): string;
+}
+
+import type { EventCommand } from "@sigureya/rpgtypes";
+
+export type CallBackFunc<
+  Command extends {
+    code: number;
+    parameters: readonly unknown[];
+    indent: number;
+  },
+  Reulst = void
+> = (
+  command: Readonly<Command>,
+  index: number,
+  list: ReadonlyArray<Readonly<EventCommand>>
+) => Reulst;
