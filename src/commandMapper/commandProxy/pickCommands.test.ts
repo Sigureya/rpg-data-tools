06cd4383844be2e3a75d9e2204539862ea18e1a8
Sigureya <sigureya1208@gmail.com>
2025-01-29 08:52:20 +0900

    テストを追加

--- src/commandMapper/commandProxy/pickCommands.test.ts
+++ src/commandMapper/commandProxy/pickCommands.test.ts
@@ -10,6 +10,8 @@ import {
   pickScrollText,
   pickCommands,
 } from "./pickCommands";
+import { SHOW_SCROLLING_TEXT } from "@sigureya/rpgtypes";
+
 import type { Command_ShowMessageBody, EventCommand } from "@sigureya/rpgtypes";
 
 describe("codeTest", () => {
@@ -64,5 +66,64 @@ describe("pickMessageWithHead", () => {
   test("not pick", () => {
     expect(pickMessageWithHead(commands, 1)).toBeUndefined();
     expect(pickMessageWithHead(commands, commands.length)).toBeUndefined();
+  });
+});
+describe("pickScripts", () => {
+  const commands: EventCommand[] = [
+    { code: 355, parameters: ["console.log('test')"], indent: 0 },
+    { code: 655, parameters: ["console.log('test2')"], indent: 0 },
+    { code: 655, parameters: ["console.log('test3')"], indent: 0 },
+  ];
+  test("pick", () => {
+    const result = pickScripts(commands, 0);
+    expect(result).not.toBeUndefined();
+    expect(result?.head.code).toBe(355);
+    expect(result?.head.parameters).toEqual(["console.log('test')"]);
+    expect(result?.bodys).toHaveLength(2);
+    expect(result?.bodys[0].parameters).toEqual(["console.log('test2')"]);
+    expect(result?.bodys[1].parameters).toEqual(["console.log('test3')"]);
+  });
+  test("not pick", () => {
+    expect(pickScripts(commands, 1)).toBeUndefined();
+    expect(pickScripts(commands, commands.length)).toBeUndefined();
+  });
+});
+
+describe("pickScrollText", () => {
+  const commands: EventCommand[] = [
+    { code: SHOW_SCROLLING_TEXT, parameters: [9, false], indent: 0 },
+    { code: 405, parameters: ["Scroll text 2"], indent: 0 },
+    { code: 405, parameters: ["Scroll text 3"], indent: 0 },
+  ];
+  test("pick", () => {
+    const result = pickScrollText(commands, 0);
+    expect(result).not.toBeUndefined();
+    expect(result?.head.code).toBe(SHOW_SCROLLING_TEXT);
+    expect(result?.bodys).toHaveLength(2);
+    expect(result?.bodys[0].parameters).toEqual(["Scroll text 2"]);
+    expect(result?.bodys[1].parameters).toEqual(["Scroll text 3"]);
+  });
+  test("not pick", () => {
+    expect(pickScrollText(commands, 1)).toBeUndefined();
+    expect(pickScrollText(commands, commands.length)).toBeUndefined();
+  });
+});
+
+describe("pickCommands", () => {
+  const commands: EventCommand[] = [
+    { code: 108, parameters: ["Command 1"], indent: 0 },
+    { code: 108, parameters: ["Command 2"], indent: 0 },
+    { code: 108, parameters: ["Command 3"], indent: 0 },
+  ];
+  test("pick", () => {
+    const result = pickCommands(108, commands, 0);
+    expect(result).toHaveLength(3);
+    expect(result[0].parameters).toEqual(["Command 1"]);
+    expect(result[1].parameters).toEqual(["Command 2"]);
+    expect(result[2].parameters).toEqual(["Command 3"]);
+  });
+  test("not pick", () => {
+    expect(pickCommands(109, commands, 0)).toHaveLength(0);
+    expect(pickCommands(108, commands, commands.length)).toHaveLength(0);
   });
 });
