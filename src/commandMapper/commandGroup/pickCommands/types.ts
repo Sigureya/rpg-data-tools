5ba84b15a9684df8b35486c146829723b8d19d22
Sigureya <sigureya1208@gmail.com>
2025-01-29 19:14:52 +0900

    組み合わせで使う型をtypeで定義。テストなどに必要

--- src/commandMapper/commandGroup/pickCommands/types.ts
+++ src/commandMapper/commandGroup/pickCommands/types.ts
@@ -1,4 +1,20 @@
-import type { EventCode, EventCommandLike } from "@sigureya/rpgtypes";
+import type {
+  Command_ShowMessage,
+  Command_ShowMessageBody,
+  Command_ShowScrollingText,
+  Command_ShowScrollingTextBody,
+  Command_Comment,
+  Command_CommentBody,
+  Command_ScriptHeader,
+  Command_ScriptBody,
+  EventCode,
+  EventCommandLike,
+} from "@sigureya/rpgtypes";
+export interface CommandReadError {
+  headCode: number;
+  bodyCode: number;
+  index: number;
+}
 
 export interface Command_TextBody<Code extends EventCode = EventCode> {
   code: Code;
@@ -14,8 +30,22 @@ export interface EventCommandPair<
   bodys: Body[];
 }
 
-export interface CommandReadError {
-  headCode: number;
-  bodyCode: number;
-  index: number;
-}
+export type EventCommandPair_Message = EventCommandPair<
+  Command_ShowMessage,
+  Command_ShowMessageBody
+>;
+
+export type EventCommandPair_ScrollingText = EventCommandPair<
+  Command_ShowScrollingText,
+  Command_ShowScrollingTextBody
+>;
+
+export type EventCommandPair_Comment = EventCommandPair<
+  Command_Comment,
+  Command_CommentBody
+>;
+
+export type EventCommandPair_Script = EventCommandPair<
+  Command_ScriptHeader,
+  Command_ScriptBody
+>;
