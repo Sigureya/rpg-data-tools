33092a81c32ba01655e227afd016a7e441af4b23
Sigureya <sigureya1208@gmail.com>
2025-01-29 18:44:36 +0900

    単語名の間違いを修正

--- src/commandMapper/text.ts
+++ src/commandMapper/text.ts
@@ -2,8 +2,11 @@ import {
   CHANGE_NAME,
   CHANGE_NICKNAME,
   CHANGE_PROFILE,
+  COMMENT,
+  SCRIPT_EVAL,
   SHOW_CHOICES,
   SHOW_CHOICES_ITEM,
+  SHOW_MESSAGE,
   SHOW_SCROLLING_TEXT,
 } from "@sigureya/rpgtypes";
 import type {
@@ -17,19 +20,30 @@ import type {
   EventCommand,
 } from "@sigureya/rpgtypes";
 import type { CallBackFunc } from "./types";
+import {
+  type EventCommandGroup_Message,
+  type EventCommandGroup_ScrollingText,
+  type EventCommandGroup_Comment,
+  type EventCommandGroup_Script,
+  handleGroupMessage,
+  handleGroupScrollingText,
+  handleGroupComment,
+  handleGroupScript,
+} from "./commandGroop";
 
 export interface TextCommandMapper<T> {
-  //  showMessage(proxy: MessageProxy): T;
+  showMessage(groop: EventCommandGroup_Message): T;
+  showScrollingText(groop: EventCommandGroup_ScrollingText): T;
+  comment(groop: EventCommandGroup_Comment): T;
+  script(groop: EventCommandGroup_Script): T;
+
   showChoices: CallBackFunc<Command_ShowChoices, T>;
   choiceWhen: CallBackFunc<Command_ChoiceWhen, T>;
-  showScrollingText: CallBackFunc<Command_ShowScrollingText, T>;
   changeName: CallBackFunc<Command_ChangeName, T>;
   changeProfile: CallBackFunc<Command_ChangeProfile, T>;
   changeNickname: CallBackFunc<Command_ChangeNickName, T>;
   commentBody: CallBackFunc<Command_CommentBody, T>;
-  // comment2: (
-  //   proxy: MeesaCommandProxy2<Command_Comment, Command_CommentBody>
-  // ) => T;
+
   other: CallBackFunc<EventCommand, T>;
 }
 
@@ -39,22 +53,24 @@ export const mapTextCommand = <T>(
 ) => {
   return list.map<T>((command, index, array) => {
     switch (command.code) {
-      // case SHOW_MESSAGE:
-      //   return table.showMessage(new MessageProxy(command, index, array));
+      case SHOW_MESSAGE:
+        return handleGroupMessage(array, index, table.showMessage);
+      case SHOW_SCROLLING_TEXT:
+        return handleGroupScrollingText(array, index, table.showScrollingText);
+      case COMMENT:
+        return handleGroupComment(array, index, table.comment);
+      case SCRIPT_EVAL:
+        return handleGroupScript(array, index, table.script);
       case SHOW_CHOICES:
         return table.showChoices(command, index, array);
       case SHOW_CHOICES_ITEM:
         return table.choiceWhen(command, index, array);
-      case SHOW_SCROLLING_TEXT:
-        return table.showScrollingText(command, index, array);
       case CHANGE_NAME:
         return table.changeName(command, index, array);
       case CHANGE_PROFILE:
         return table.changeProfile(command, index, array);
       case CHANGE_NICKNAME:
         return table.changeNickname(command, index, array);
-      // case COMMENT:
-      //   return table.comment2(createComment(index, array));
 
       default:
         return table.other(command, index, array);
