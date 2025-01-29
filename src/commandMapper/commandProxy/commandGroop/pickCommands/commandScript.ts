48ab9b52a4fbe915811e23a2122180538ac996be
Sigureya <sigureya1208@gmail.com>
2025-01-29 17:31:00 +0900

    エラー処理を修正。undefinedではなく例外にした

--- src/commandMapper/commandProxy/commandGroop/pickCommands/commandScript.ts
+++ src/commandMapper/commandProxy/commandGroop/pickCommands/commandScript.ts
@@ -4,18 +4,16 @@ import type {
   Command_ScriptBody,
 } from "@sigureya/rpgtypes";
 import { SCRIPT_EVAL, SCRIPT_EVAL_BODY } from "@sigureya/rpgtypes";
-import { isHeadCommand, pickCommands } from "./pickCommands";
+import { pickCommands, pickHead } from "./pickCommands";
 import type { EventCommandPair } from "./types";
 
 export const pickScripts = (
   arrya: ReadonlyArray<EventCommand>,
   start: number
 ): EventCommandPair<Command_ScriptHeader, Command_ScriptBody> | undefined => {
-  const head = arrya[start];
-  if (isHeadCommand(SCRIPT_EVAL, head)) {
-    return {
-      head: head,
-      bodys: pickCommands(SCRIPT_EVAL_BODY, arrya, start + 1),
-    };
-  }
+  const head = pickHead(arrya, start, SCRIPT_EVAL);
+  return {
+    head: head,
+    bodys: pickCommands(SCRIPT_EVAL_BODY, arrya, start + 1),
+  };
 };
--- src/commandMapper/commandProxy/commandGroop/pickCommands/errorConstants.ts
+++ src/commandMapper/commandProxy/commandGroop/pickCommands/errorConstants.ts
@@ -1,1 +1,3 @@
 export const ERROR_COMMAND_MESSAGE = "MessageHeader invalid command" as const;
+export const ERROR_COMMAND_SCRIPT = "Script invalid command" as const;
+export const ERROR_INVALID_HEAD = "Invalid Head" as const;
