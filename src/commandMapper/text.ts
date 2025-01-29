de6f637064fee462ad09f5c2b3a99ceb14417e3c
Sigureya <sigureya1208@gmail.com>
2025-01-29 22:16:43 +0900

    関数の呼び出し方法を修正。これでmap以外にも対応可能になった

--- src/commandMapper/text.ts
+++ src/commandMapper/text.ts
@@ -18,34 +18,43 @@ import {
 } from "./commandGroup";
 import type { TextCommandMapper } from "./textCommandMapper";
 
+export const handlerDispatch = <T>(
+  array: ReadonlyArray<EventCommand>,
+  index: number,
+  table: TextCommandMapper<T>
+): T => {
+  const command = array[index];
+  switch (command.code) {
+    case SHOW_MESSAGE:
+      return handleGroupMessage(array, index, table.showMessage);
+    case SHOW_SCROLLING_TEXT:
+      return handleGroupScrollingText(array, index, table.showScrollingText);
+    case COMMENT:
+      return handleGroupComment(array, index, table.comment);
+    case SCRIPT_EVAL:
+      return handleGroupScript(array, index, table.script);
+
+    case SHOW_CHOICES:
+      return table.showChoices(command, index, array);
+    case SHOW_CHOICES_ITEM:
+      return table.choiceWhen(command, index, array);
+    case CHANGE_NAME:
+      return table.changeName(command, index, array);
+    case CHANGE_PROFILE:
+      return table.changeProfile(command, index, array);
+    case CHANGE_NICKNAME:
+      return table.changeNickname(command, index, array);
+
+    default:
+      return table.other(command, index, array);
+  }
+};
+
 export const mapTextCommand = <T>(
   list: ReadonlyArray<EventCommand>,
   table: TextCommandMapper<T>
 ) => {
-  return list.map<T>((command, index, array) => {
-    switch (command.code) {
-      case SHOW_MESSAGE:
-        return handleGroupMessage(array, index, table.showMessage);
-      case SHOW_SCROLLING_TEXT:
-        return handleGroupScrollingText(array, index, table.showScrollingText);
-      case COMMENT:
-        return handleGroupComment(array, index, table.comment);
-      case SCRIPT_EVAL:
-        return handleGroupScript(array, index, table.script);
-
-      case SHOW_CHOICES:
-        return table.showChoices(command, index, array);
-      case SHOW_CHOICES_ITEM:
-        return table.choiceWhen(command, index, array);
-      case CHANGE_NAME:
-        return table.changeName(command, index, array);
-      case CHANGE_PROFILE:
-        return table.changeProfile(command, index, array);
-      case CHANGE_NICKNAME:
-        return table.changeNickname(command, index, array);
-
-      default:
-        return table.other(command, index, array);
-    }
-  });
+  return list.map<T>((command, index, array) =>
+    handlerDispatch(array, index, table)
+  );
 };
