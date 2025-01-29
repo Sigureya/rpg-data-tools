aaa17eda7495d13efc4248a402f8dafe43aad45c
Sigureya <sigureya1208@gmail.com>
2025-01-29 17:47:23 +0900

    旧型のグループクラスを削除。あとimportの整理

--- dist/commandMapper/commandProxy/onGroop.d.ts
+++ dist/commandMapper/commandProxy/onGroop.d.ts
@@ -1,5 +1,5 @@
 import { EventCommand } from '@sigureya/rpgtypes';
-import { EventCommandGroup_Comment, EventCommandGroup_Message, EventCommandGroup_Script, EventCommandGroup_ScrollingText } from './commandGroop/types';
+import { EventCommandGroup_Comment, EventCommandGroup_Message, EventCommandGroup_Script, EventCommandGroup_ScrollingText } from './commandGroop/';
 export declare const onMessage: <Result>(array: ReadonlyArray<EventCommand>, index: number, func: (groop: EventCommandGroup_Message) => Result) => Result;
 export declare const onScrollingText: <Result>(array: ReadonlyArray<EventCommand>, index: number, func: (groop: EventCommandGroup_ScrollingText) => Result) => Result;
 export declare const onComment: <Result>(array: ReadonlyArray<EventCommand>, index: number, func: (groop: EventCommandGroup_Comment) => Result) => Result;
--- dist/commandMapper/commandProxy/proxy.d.ts
+++
@@ -1,2 +0,0 @@
-import { EventCode, EventCommand, EventCommandByCode } from '@sigureya/rpgtypes';
-export declare const create: <BodyCode extends EventCode, HeadCode extends EventCode>(array: ReadonlyArray<EventCommand>, index: number, headCode: HeadCode, bodyCode: BodyCode) => (EventCommandByCode[HeadCode] | EventCommandByCode[BodyCode])[];
