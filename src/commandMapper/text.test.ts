172f8dc75465c30669a92e6a821476d74f12a8e1
Sigureya <sigureya1208@gmail.com>
2025-01-29 22:49:12 +0900

    対応したイベントコマンドが呼び出されているかのテスト

--- src/commandMapper/text.test.ts
+++ src/commandMapper/text.test.ts
@@ -1,7 +1,22 @@
-import { vi, type MockedObject } from "vitest";
+import {
+  beforeEach,
+  describe,
+  expect,
+  test,
+  vi,
+  type MockedObject,
+} from "vitest";
 import type { TextCommandMapper } from "./textCommandMapper";
+import type {
+  Command_ChangeName,
+  Command_ChangeNickName,
+  Command_ChangeProfile,
+  Command_ShowChoices,
+  EventCommand,
+} from "@sigureya/rpgtypes";
+import { handlerDispatch } from "./text";
 
-const mockMapper: MockedObject<TextCommandMapper<void>> = {
+const createMockMapper = (): MockedObject<TextCommandMapper<void>> => ({
   showMessage: vi.fn(),
   showScrollingText: vi.fn(),
   comment: vi.fn(),
@@ -14,4 +29,57 @@ const mockMapper: MockedObject<TextCommandMapper<void>> = {
   changeNickname: vi.fn(),
   commentBody: vi.fn(),
   other: vi.fn(),
-};
+});
+describe("handlerDispatch", () => {
+  let mapper: MockedObject<TextCommandMapper<void>>;
+  beforeEach(() => (mapper = createMockMapper()));
+
+  test("ShowChoices", () => {
+    const command: Command_ShowChoices = {
+      code: 102,
+      parameters: [[], 0, 0, 0, 0],
+      indent: 0,
+    };
+    handlerDispatch([command], 0, mapper);
+    expect(mapper.showChoices).toHaveBeenCalledWith(command, 0, [command]);
+  });
+  test("ChangeName", () => {
+    const command: Command_ChangeName = {
+      code: 320,
+      parameters: [1, "name"],
+      indent: 0,
+    };
+    mapper.changeName.mockReturnValue(undefined);
+    handlerDispatch([command], 0, mapper);
+    expect(mapper.changeName).toHaveBeenCalledWith(command, 0, [command]);
+  });
+
+  test("ChangeProfile", () => {
+    const command: Command_ChangeProfile = {
+      code: 325,
+      parameters: [1, "profile"],
+      indent: 0,
+    };
+    mapper.changeProfile.mockReturnValue(undefined);
+    handlerDispatch([command], 0, mapper);
+    expect(mapper.changeProfile).toHaveBeenCalledWith(command, 0, [command]);
+  });
+
+  test("changeNickname", () => {
+    const command: Command_ChangeNickName = {
+      code: 324,
+      parameters: [1, "nickname"],
+      indent: 0,
+    };
+    mapper.changeNickname.mockReturnValue(undefined);
+    handlerDispatch([command], 0, mapper);
+    expect(mapper.changeNickname).toHaveBeenCalledWith(command, 0, [command]);
+  });
+
+  test("other", () => {
+    const command: EventCommand = { code: 0, parameters: [], indent: 0 };
+    mapper.other.mockReturnValue(undefined);
+    handlerDispatch([command], 0, mapper);
+    expect(mapper.other).toHaveBeenCalledWith(command, 0, [command]);
+  });
+});
