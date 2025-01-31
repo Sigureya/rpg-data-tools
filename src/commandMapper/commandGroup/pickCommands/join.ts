0e9d7be60ec2ff19101e0d1aa914d6219082d46d
Sigureya <sigureya1208@gmail.com>
2025-01-31 10:12:10 +0900

    イベントコマンドの結合処理を別フォルダに移動。このフォルダを基準にする

--- src/commandMapper/commandGroup/pickCommands/join.ts
+++
@@ -1,8 +0,0 @@
-import type { Command_TextBody } from "./types";
-
-export const joinCommandBodies = <Command extends Command_TextBody>(
-  array: ReadonlyArray<Command>,
-  sepadrator = "\n"
-) => {
-  return array.map((v) => v.parameters[0]).join(sepadrator);
-};
