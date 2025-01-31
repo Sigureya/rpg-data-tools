cc70d10ed43ad7dece12e87676073901cd03eb86
Sigureya <sigureya1208@gmail.com>
2025-01-31 18:03:23 +0900

    テストコードを一時保存

--- src/commandMapper/groupMerge.test.ts
+++ src/commandMapper/groupMerge.test.ts
@@ -2,16 +2,32 @@ import type * as Types from "@sigureya/rpgtypes";
 import { describe, expect, test } from "vitest";
 import { normalizedCommands } from "./groupMerge";
 
+const MockJoinedText = "ふるいけや\nかわずとびこむ\nみずのおと" as const;
+const createMockCommand = <
+  Code extends Types.PickCommandByParam<[string]>["code"]
+>(
+  code: Code,
+  textList = ["ふるいけや", "かわずとびこむ", "みずのおと"]
+) =>
+  textList.map((s) => ({
+    code,
+    indent: 0,
+    parameters: [s] as [string],
+  }));
+
+const sss = (name: string, command: Types.PickCommandByParam<[string]>) => {
+  test(`code:${command.code} ${name}`, () => {
+    const result: Types.EventCommand[][] = normalizedCommands([command]);
+    expect(result).toEqual([]);
+  });
+};
+
 describe("groupJoin", () => {
   describe("body単体は消せるか？", () => {
-    test("commentBody", () => {
-      const mockComment: Types.Command_CommentBody = {
-        code: 408,
-        indent: 0,
-        parameters: ["test"],
-      };
-      const result = normalizedCommands([mockComment]);
-      expect(result).toEqual([]);
+    sss("showMessageBody", {
+      code: 401,
+      indent: 0,
+      parameters: ["abcd"],
     });
     test("scrollingTextBody", () => {
       const mockScrollingText: Types.Command_ShowScrollingTextBody = {
@@ -19,20 +35,53 @@ describe("groupJoin", () => {
         indent: 0,
         parameters: ["abcd"],
       };
-      const result = normalizedCommands([mockScrollingText]);
-      expect(result).toEqual([]);
+      const result: Types.EventCommand[][] = normalizedCommands([
+        mockScrollingText,
+      ]);
+      expect(result.flat()).toEqual([]);
     });
   });
-
-  describe("case showMessage", () => {
-    test("showMessage", () => {
-      const command: Types.Command_ShowMessage = {
-        code: 101,
+});
+describe("case showMessage", () => {
+  test("showMessage", () => {
+    const command: Types.Command_ShowMessage = {
+      code: 101,
+      indent: 0,
+      parameters: ["test", 0, 0, 0, "speaker"],
+    };
+    const bodies: Types.Command_ShowMessageBody[] = createMockCommand(401);
+    const result: Types.EventCommand[][] = normalizedCommands([
+      command,
+      ...bodies,
+    ]);
+    expect<Types.EventCommand[]>(result.flat()).toMatchObject([
+      command,
+      {
+        code: 401,
         indent: 0,
-        parameters: ["test", 0, 0, 0, "speaker"],
-      };
-      const result = normalizedCommands([command]);
-      expect(result).toEqual([command]);
-    });
+        parameters: [MockJoinedText],
+      } as Types.Command_ShowMessageBody,
+    ]);
+  });
+});
+describe("case comment", () => {
+  test("comment", () => {
+    const command: Types.Command_Comment = {
+      code: 108,
+      indent: 0,
+      parameters: ["松尾芭蕉"],
+    };
+    const bodies: Types.Command_CommentBody[] = createMockCommand(408);
+    const result: Types.EventCommand[][] = normalizedCommands([
+      command,
+      ...bodies,
+    ]);
+    expect(result.flat()).toMatchObject([
+      {
+        code: 108,
+        indent: 0,
+        parameters: ["松尾芭蕉\n" + MockJoinedText],
+      } as Types.Command_Comment,
+    ]);
   });
 });
