b93f251b6ee65396d5ffa56bae1bbc1795949bda
Sigureya <sigureya1208@gmail.com>
2025-01-31 11:05:17 +0900

    不要なimportを削除

--- src/commandMapper/commandGroup/pickCommands/types.ts
+++ src/commandMapper/commandGroup/pickCommands/types.ts
@@ -1,16 +1,3 @@
-import type {
-  Command_ShowMessage,
-  Command_ShowMessageBody,
-  Command_ShowScrollingText,
-  Command_ShowScrollingTextBody,
-  Command_Comment,
-  Command_CommentBody,
-  Command_ScriptHeader,
-  Command_ScriptBody,
-  EventCode,
-  EventCommandLike,
-  EventCommand,
-} from "@sigureya/rpgtypes";
 export interface CommandReadError {
   headCode: number;
   bodyCode: number;
