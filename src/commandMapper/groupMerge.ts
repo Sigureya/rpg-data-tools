bf6aff82b3c3cde88ab26762bc99e816e05649c2
Sigureya <sigureya1208@gmail.com>
2025-01-31 20:01:59 +0900

    選択肢ヘルプに対する特別対応

--- src/commandMapper/groupMerge.ts
+++ src/commandMapper/groupMerge.ts
@@ -1,6 +1,8 @@
-import type * as RpgTypes from "@sigureya/rpgtypes";
+import * as RpgTypes from "@sigureya/rpgtypes";
 import type { MappingObject } from "./allCommandsMapper";
 import { mappingCommand } from "./allMapping";
+import type { EventCommandGroup_Comment } from "./commandGroup";
+import { joinCommandBodies } from "./commandGroup";
 
 export interface GroupJoinMapper<T>
   extends Pick<
@@ -15,6 +17,33 @@ export interface GroupJoinMapper<T>
     | "showScrollingTextBody"
     | "other"
   > {}
+/**
+ * @description 『MPP_ChoiceEx.js』に対応するための特殊文字列
+ */
+export const CHOICE_HELP_TEXT = "選択肢ヘルプ" as const;
+
+export const is2 = (command: RpgTypes.Command_Comment) => {
+  return command.parameters[0] === CHOICE_HELP_TEXT;
+};
+
+const mergeComment = (comment: EventCommandGroup_Comment) => {
+  if (is2(comment.header)) {
+    const text: string = joinCommandBodies(comment.bodies);
+    return [
+      {
+        code: RpgTypes.COMMENT,
+        indent: 0,
+        parameters: [CHOICE_HELP_TEXT],
+      } as RpgTypes.Command_Comment,
+      {
+        code: RpgTypes.COMMENT_BODY,
+        indent: 0,
+        parameters: [text],
+      } as RpgTypes.Command_CommentBody,
+    ];
+  }
+  return comment.normalizedCommands();
+};
 
 const groupJoinMapper: GroupJoinMapper<RpgTypes.EventCommand[]> = {
   // body部分は空の要素で置き換える
@@ -25,7 +54,7 @@ const groupJoinMapper: GroupJoinMapper<RpgTypes.EventCommand[]> = {
   // ヘッダ側を基準に、Header+Bodyで生成
   showMessage: (data) => data.normalizedCommands(),
   showScrollingText: (data) => data.normalizedCommands(),
-  comment: (data) => data.normalizedCommands(),
+  comment: (data) => mergeComment(data),
   script: (data) => data.normalizedCommands(),
   other: (data) => [data],
 };
