fd7f3a3757242c879732bf21bc67b55203cf7f91
Sigureya <sigureya1208@gmail.com>
2025-02-01 11:04:38 +0900

    RPGツクール標準のマップデータを部分的に扱う型

---
+++ src/eventCommandEdit/rpg/map/types.ts
@@ -0,0 +1,22 @@
+import type { EventCommand } from "@sigureya/rpgtypes";
+
+export interface EventContainer {
+  list: EventCommand[];
+}
+export interface EventPageContainer {
+  pages: EventContainer[];
+}
+
+export interface MapEventLike {
+  id: number;
+  pages: EventContainer[];
+}
+
+export interface MapLike {
+  events: (MapEventLike | null)[];
+}
+export interface MapEventContext {
+  eventId: number;
+  pageIndex: number;
+  command: EventCommand;
+}
