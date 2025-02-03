7cb71ddadb3198cc173af84ee04f3c6330035583
Sigureya <sigureya1208@gmail.com>
2025-02-04 00:06:26 +0900

    音声抽出を削除。別リポジトリへの移動が完了した為

--- dist/resourcePath/audio/fromCommand.d.ts
+++
@@ -1,7 +0,0 @@
-import { AudioResourcePath } from './types';
-import { AudioFileParams, ExtractCommandByParam } from '@sigureya/rpgtypes';
-export declare const isAudioResource: (path: {
-    folder: string;
-    fileName: string;
-}) => path is AudioResourcePath;
-export declare const audioFromCommands: <T extends ExtractCommandByParam<[AudioFileParams]>>(command: T) => AudioResourcePath;
