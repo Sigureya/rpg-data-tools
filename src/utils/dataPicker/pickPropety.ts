8b95d04b616566670ce14d6af5d2e71111c7acb3
Sigureya <sigureya1208@gmail.com>
2025-02-05 14:31:16 +0900

    pickString()を修正。型定義が以前よりもシンプルになった

--- src/utils/dataPicker/pickPropety.ts
+++ src/utils/dataPicker/pickPropety.ts
@@ -1,28 +1,26 @@
 import type { PickByType } from "@sigureya/rpgtypes";
 
+export type PickableRecordType<T, Value> = Record<
+  string & keyof PickByType<T, Value>,
+  Value
+>;
+
 export const pickPropertys = <
   Result,
   Value,
-  T extends Record<KeyType, Value> = Record<string, Value>,
-  KeyType extends string & keyof PickByType<T, Value> = string &
-    keyof PickByType<T, Value>
+  T extends PickableRecordType<T, Value>
 >(
   data: T,
-  keyList: ReadonlyArray<string & keyof PickByType<T, Value>>,
+  keyList: ReadonlyArray<keyof PickableRecordType<T, string>>,
   fn: (key: string, value: Value, data: T) => Result
 ): Result[] => {
   return keyList.map((key) => fn(key, data[key], data));
 };
 
-export const pickString = <
-  Result,
-  T extends Record<KeyType, string> = Record<string, string>,
-  KeyType extends string & keyof PickByType<T, string> = string &
-    keyof PickByType<T, string>
->(
+export const pickString = <Result, T extends PickableRecordType<T, string>>(
   data: T,
-  keyList: ReadonlyArray<KeyType>,
+  keyList: ReadonlyArray<keyof PickableRecordType<T, string>>,
   fn: (key: string, value: string, data: T) => Result
 ): Result[] => {
-  return pickPropertys<Result, string, T>(data, keyList, fn);
+  return pickPropertys(data, keyList, fn);
 };
