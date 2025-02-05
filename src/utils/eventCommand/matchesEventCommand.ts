ff23643bde7735d02ee6c6a43406791a58450d9b
Sigureya <sigureya1208@gmail.com>
2025-02-05 20:38:12 +0900

    イベントコマンドの種別判定。filter()と組み合わせて使う

---
+++ src/utils/eventCommand/matchesEventCommand.ts
@@ -0,0 +1,40 @@
+import type {
+  AudioFileParams,
+  EventCode,
+  EventCommand,
+  ExtractCommandByParam,
+} from "@sigureya/rpgtypes";
+import * as RpgTypes from "@sigureya/rpgtypes";
+
+export const matchesEventCommand = <Code extends EventCode>(
+  command: EventCommand,
+  codeList: ReadonlyArray<Code>
+): command is Extract<EventCommand, { code: Code }> => {
+  return codeList.includes(command.code as Code);
+};
+export const isAudioCommand = (
+  command: EventCommand
+): command is ExtractCommandByParam<[AudioFileParams]> =>
+  matchesEventCommand(command, AUDIO_CODE);
+
+const AUDIO_CODE: ReadonlyArray<
+  ExtractCommandByParam<[AudioFileParams]>["code"]
+> = [
+  RpgTypes.PLAY_BGM,
+  RpgTypes.PLAY_BGS,
+  RpgTypes.PLAY_ME,
+  RpgTypes.PLAY_SE,
+  RpgTypes.CHANGE_BATTLE_BGM,
+  RpgTypes.CHANGE_VICTORY_ME,
+  RpgTypes.CHANGE_DEFEAT_ME,
+];
+export const isImageCommand = (command: EventCommand) =>
+  matchesEventCommand(command, IMAGE_CODE);
+
+const IMAGE_CODE = [
+  RpgTypes.SHOW_PICTURE,
+  RpgTypes.CHANGE_ACTOR_IMAGES,
+  RpgTypes.CHANGE_VEHICLE_IMAGE,
+  RpgTypes.CHANGE_PARALLAX,
+  RpgTypes.CHANGE_BATTLE_BACKGROUND,
+];
