c8364e8f2bbe26d964618d349c6dcea11f555eb3
Sigureya <sigureya1208@gmail.com>
2025-02-09 04:55:31 +0900

    変数IDの指定箇所を示すテーブル

--- src/eventCommand/variable/variableDesignation/piravate/privateConstants.ts
+++ src/eventCommand/variable/variableDesignation/piravate/privateConstants.ts
@@ -1,10 +1,22 @@
-import RpgTypes from "@sigureya/rpgtypes";
+import * as RpgTypes from "@sigureya/rpgtypes";
 import type { Table_IndexOfDesignation as Table_IndexOfDesignation } from "./privateTypes";
 
 export const VARIABLE_DESIGNATION = 1 as const;
 
-export const INDEX_TABLE: Table_IndexOfDesignation = {
+export const DESIGNATION_INDEX_TABLE: Table_IndexOfDesignation = {
   [RpgTypes.CHANGE_ENEMY_HP]: 2,
   [RpgTypes.CHANGE_ENEMY_MP]: 2,
   [RpgTypes.TRANSFER_PLAYER]: 0,
+  [RpgTypes.SET_VEHICLE_LOCATION]: 1,
+  [RpgTypes.GET_LOCATION_INFO]: 2,
+  [RpgTypes.BATTLE_PROCESSING]: 0,
+} as const;
+
+export const VARIABLEID_INDEX_TABLE = {
+  [RpgTypes.CHANGE_ENEMY_HP]: 4,
+  [RpgTypes.CHANGE_ENEMY_MP]: 2,
+  [RpgTypes.TRANSFER_PLAYER]: 1,
+  [RpgTypes.SET_VEHICLE_LOCATION]: 2,
+  [RpgTypes.GET_LOCATION_INFO]: 3,
+  [RpgTypes.BATTLE_PROCESSING]: 1,
 } as const;
