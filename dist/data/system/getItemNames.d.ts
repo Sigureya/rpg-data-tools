3b8ea295d470fdda663eeca65c6c0c1a914709f7
Sigureya <sigureya1208@gmail.com>
2025-04-08 01:43:59 +0900

    ラベル関連の処理などを追加してビルド

---
+++ dist/data/system/getItemNames.d.ts
@@ -0,0 +1,11 @@
+import { Data_NamedItem, Data_System, System_ParamNames } from '@sigureya/rpgtypes';
+export declare const getVariableNames: (system: Pick<Data_System, "variables">) => Data_NamedItem[];
+export declare const getElements: (system: Pick<Data_System, "elements">) => Data_NamedItem[];
+export declare const getEquipTypes: (system: Pick<Data_System, "equipTypes">) => Data_NamedItem[];
+export declare const getSkillTypes: (system: Pick<Data_System, "skillTypes">) => Data_NamedItem[];
+export declare const getWeaponTypes: (system: Pick<Data_System, "weaponTypes">) => Data_NamedItem[];
+export declare const getArmorTypes: (system: Pick<Data_System, "armorTypes">) => Data_NamedItem[];
+export declare const getParamNames: (system: System_ParamNames) => {
+    id: number;
+    name: string;
+}[];
--- dist/labels/battlerParam/makeOptions.d.ts
+++ dist/labels/battlerParam/makeOptions.d.ts
@@ -2,5 +2,5 @@ import { CollapsOptionLabels, Data_NamedItem, ExtraParamLabels, PartyAbilityOpti
 export declare const foldCollapsOptions: (options: CollapsOptionLabels, global: Pick<GlobalLabel, "normal">) => Data_NamedItem[];
 export declare const foldPartyAbilityOptions: (options: PartyAbilityOptionLabels) => Data_NamedItem[];
 export declare const foldRegularParam: (param: RegularParamLabels) => Data_NamedItem[];
-export declare const foldXParam: (xparam: ExtraParamLabels) => Data_NamedItem[];
+export declare const foldExtraParam: (xparam: ExtraParamLabels) => Data_NamedItem[];
 export declare const foldSpecialParams: (specialParams: SpecialParamLabels) => Data_NamedItem[];
