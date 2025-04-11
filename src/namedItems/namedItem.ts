2cd6cba15fee3875defe5f63a0f8a84f6856112a
Sigureya <sigureya1208@gmail.com>
2025-04-11 15:48:20 +0900

    trait関連への対応を追加

--- src/namedItems/namedItem.ts
+++ src/namedItems/namedItem.ts
@@ -1,27 +1,33 @@
 import type {
   Data_NamedItem,
-  Data_System,
-  DataTypesTable,
   GlobalLabel,
   System_DataNames,
+  TraitLabels,
 } from "@sigureya/rpgtypes";
 import type { NamedItemList } from "./listSource";
 import {
-  INDEX_SOURCE_ACTOR,
+  INDEX_SOURCE_DATA_ACTOR,
   INDEX_SOURCE_ARMOR,
   INDEX_SOURCE_ARMOR_TYPES,
   INDEX_SOURCE_CLASS,
   INDEX_SOURCE_COMMON_EVNET,
   INDEX_SOURCE_ELEMENTS,
-  INDEX_SOURCE_ENEMY,
+  INDEX_SOURCE_DATA_ENEMY,
   INDEX_SOURCE_EQUIP_TYPES,
   INDEX_SOURCE_ITEMS,
+  INDEX_SOURCE_PARAMS,
+  INDEX_SOURCE_TRAIT_COLLAPS,
+  INDEX_SOURCE_PARAMS_EXTRA,
+  INDEX_SOURCE_PARAMS_SPECIAL,
   INDEX_SOURCE_SKILL,
   INDEX_SOURCE_SKILL_TYPES,
-  INDEX_SOURCE_STATE,
+  INDEX_SOURCE_DATA_STATE,
   INDEX_SOURCE_TROOP,
-  INDEX_SOURCE_VARIABLE,
+  INDEX_SOURCE_DATA_VARIABLE,
   INDEX_SOURCE_WEAPON_TYPES,
+  INDEX_SOURCE_TRAIT_PARTY_ABILITY,
+  INDEX_SOURCE_TRAIT_SPECIAL_FLAG,
+  INDEX_SOURCE_WEAPON,
 } from "./listSource";
 import {
   getArmorTypes,
@@ -33,6 +39,13 @@ import {
 } from "./data";
 import type { GlobalGameDataNamedItems } from "./data/main/types";
 import type { MainDataDomains } from "./data/main/domains";
+import {
+  foldCollapsOptions,
+  foldExtraParam,
+  foldPartyAbilityOptions,
+  foldRegularParam,
+  foldSpecialParams,
+} from "./traits";
 
 interface NamedItems_System {
   armorTypes: NamedItemList;
@@ -82,7 +95,7 @@ export const systemData = (
   },
   variables: {
     items: getVariableNames(system),
-    key: INDEX_SOURCE_VARIABLE,
+    key: INDEX_SOURCE_DATA_VARIABLE,
     label: labels.variables,
   },
   weaponTypes: {
@@ -98,10 +111,10 @@ const namedItems = (itemList: Data_NamedItem[]): Data_NamedItem[] =>
 export const mainData = (
   gameData: GlobalGameDataNamedItems,
   domains: MainDataDomains
-): Partial<XX<GlobalGameDataNamedItems>> => ({
+): XX<GlobalGameDataNamedItems> => ({
   actors: {
     items: namedItems(gameData.actors),
-    key: INDEX_SOURCE_ACTOR,
+    key: INDEX_SOURCE_DATA_ACTOR,
     label: domains.actors.domainName,
   },
   armors: {
@@ -121,7 +134,7 @@ export const mainData = (
   },
   enemies: {
     items: namedItems(gameData.enemies),
-    key: INDEX_SOURCE_ENEMY,
+    key: INDEX_SOURCE_DATA_ENEMY,
     label: domains.enemies.domainName,
   },
   items: {
@@ -136,12 +149,53 @@ export const mainData = (
   },
   states: {
     items: namedItems(gameData.states),
-    key: INDEX_SOURCE_STATE,
+    key: INDEX_SOURCE_DATA_STATE,
     label: domains.states.domainName,
   },
   troops: {
     items: namedItems(gameData.troops),
     key: INDEX_SOURCE_TROOP,
     label: domains.troops.domainName,
+  },
+  weapons: {
+    items: namedItems(gameData.weapons),
+    key: INDEX_SOURCE_WEAPON,
+    label: domains.weapons.domainName,
+  },
+});
+
+export const trait = (
+  globalLabel: Pick<GlobalLabel, "normal">,
+  labels: TraitLabels
+): XX<TraitLabels["options"]> => ({
+  paramRegular: {
+    key: INDEX_SOURCE_PARAMS,
+    label: labels.types.regularParam,
+    items: foldRegularParam(labels.options.paramRegular),
+  },
+  paramExtra: {
+    key: INDEX_SOURCE_PARAMS_EXTRA,
+    label: labels.types.extraParam,
+    items: foldExtraParam(labels.options.paramExtra),
+  },
+  paramSpecial: {
+    key: INDEX_SOURCE_PARAMS_SPECIAL,
+    label: labels.types.specialParam,
+    items: foldSpecialParams(labels.options.paramSpecial),
+  },
+  collaps: {
+    key: INDEX_SOURCE_TRAIT_COLLAPS,
+    label: labels.types.collapseType,
+    items: foldCollapsOptions(labels.options.collaps, globalLabel),
+  },
+  partyAbility: {
+    key: INDEX_SOURCE_TRAIT_PARTY_ABILITY,
+    label: labels.types.partyAbility,
+    items: foldPartyAbilityOptions(labels.options.partyAbility),
+  },
+  specialFlag: {
+    key: INDEX_SOURCE_TRAIT_SPECIAL_FLAG,
+    label: labels.types.specialFlag,
+    items: [],
   },
 });
