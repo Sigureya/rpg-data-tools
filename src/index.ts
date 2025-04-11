49e03f26dd8baf24c329a3114aa63c23d11db95e
Sigureya <sigureya1208@gmail.com>
2025-04-11 09:17:31 +0900

    nameとidで構成されているペアを扱うセット

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from "./constructData";
 export * from "./eventCommand";
 export * from "./format";
 export * from "./utils/";
-export * from "./data";
 export * from "./labels/";
+export * from "./namedItems";
