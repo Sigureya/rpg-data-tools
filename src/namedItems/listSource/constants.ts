7b87012e2788da0bf37a8c99996f487aec91d934
Sigureya <sigureya1208@gmail.com>
2025-04-12 09:41:21 +0900

    定数名を修正

--- src/namedItems/listSource/constants.ts
+++ src/namedItems/listSource/constants.ts
@@ -1,30 +1,30 @@
 // TODO:定数名は暫定的なもの
 // おそらく変えた方がいい
 
-export const INDEX_SOURCE_PARAMS = "params" as const;
-export const INDEX_SOURCE_PARAMS_EXTRA = "xparams" as const;
-export const INDEX_SOURCE_PARAMS_SPECIAL = "sparams" as const;
-export const INDEX_SOURCE_TRAIT_COLLAPS = "collaps" as const;
-export const INDEX_SOURCE_TRAIT_PARTY_ABILITY = "partyAbiility" as const;
-export const INDEX_SOURCE_TRAIT_SPECIAL_FLAG = "sflag";
+export const SRC_PARAMS_REGULAR = "params" as const;
+export const SRC_PARAMS_EXTRA = "xparams" as const;
+export const SRC_PARAMS_SPECIAL = "sparams" as const;
+export const SRC_TRAIT_COLLAPS = "collaps" as const;
+export const SRC_TRAIT_PARTY_ABILITY = "partyAbiility" as const;
+export const SRC_TRAIT_SPECIAL_FLAG = "sflag";
 
-export const INDEX_SOURCE_DATA_ACTOR = "actors" as const;
-export const INDEX_SOURCE_DATA_VARIABLE = "variables" as const;
-export const INDEX_SOURCE_MAP = "map" as const;
-export const INDEX_SOURCE_DATA_ENEMY = "enemy" as const;
-export const INDEX_SOURCE_DATA_STATE = "state" as const;
-export const INDEX_SOURCE_SKILL = "skill" as const;
-export const INDEX_SOURCE_ITEMS = "items" as const;
-export const INDEX_SOURCE_WEAPON = "weapon" as const;
-export const INDEX_SOURCE_ARMOR = "armor" as const;
-export const INDEX_SOURCE_CLASS = "class" as const;
-export const INDEX_SOURCE_COMMON_EVNET = "common_event" as const;
-export const INDEX_SOURCE_TROOP = "troop" as const;
-export const INDEX_SOURCE_COLOR = "colors" as const;
+export const SRC_DATA_ACTOR = "actors" as const;
+export const SRC_DATA_VARIABLE = "variables" as const;
+export const SRC_DATA_MAP = "map" as const;
+export const SRC_DATA_ENEMY = "enemy" as const;
+export const SRC_DATA_STATE = "state" as const;
+export const SRC_DATA_SKILL = "skill" as const;
+export const SRC_DATA_ITEMS = "items" as const;
+export const SRC_DATA_WEAPON = "weapon" as const;
+export const SRC_DATA_ARMOR = "armor" as const;
+export const SRC_DATA_CLASS = "class" as const;
+export const SRC_COMMON_EVNET = "common_event" as const;
+export const SRC_TROOP = "troop" as const;
+export const SRC_COLOR = "colors" as const;
 
-export const INDEX_SOURCE_WEAPON_TYPES = "weaponTypes" as const;
-export const INDEX_SOURCE_ARMOR_TYPES = "armorTypes" as const;
-export const INDEX_SOURCE_EQUIP_TYPES = "equipTypes" as const;
-export const INDEX_SOURCE_SWITCHES = "switches" as const;
-export const INDEX_SOURCE_SKILL_TYPES = "skillTypes" as const;
-export const INDEX_SOURCE_ELEMENTS = "elements" as const;
+export const SRC_WEAPON_TYPES = "weaponTypes" as const;
+export const SRC_ARMOR_TYPES = "armorTypes" as const;
+export const SRC_EQUIP_TYPES = "equipTypes" as const;
+export const SRC_SWITCHES = "switches" as const;
+export const SRC_SKILL_TYPES = "skillTypes" as const;
+export const SRC_ELEMENTS = "elements" as const;
