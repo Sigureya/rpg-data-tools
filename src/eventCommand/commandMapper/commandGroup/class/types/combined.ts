70dedb06db7094dd697f0b7167480e8286ac3cd9
Sigureya <sigureya1208@gmail.com>
2025-05-15 03:50:47 +0900

    実装の継承をやめる。これで一目で実行内容を理解できるようになった

--- src/eventCommand/commandMapper/commandGroup/class/types/combined.ts
+++ src/eventCommand/commandMapper/commandGroup/class/types/combined.ts
@@ -1,12 +1,19 @@
-import { BaseEventCommandGroup } from "./base";
+import type { EventCommandGroup } from "./groopTypes";
+import { joinHeaderAndBody } from "./join";
 import type { TextCommandBody } from "./textCommandBody";
 
+// 注釈やコメントなどの先頭とbodyが同じパラメータ型のパターン
 export class CombinedEventCommandGroup<
   Header extends TextCommandBody,
   Body extends TextCommandBody
-> extends BaseEventCommandGroup<Header, Body> {
-  protected getExpandedBodies(): [Header, ...Body[]] {
-    return [this.header, ...this.bodies];
+> implements EventCommandGroup<Header, Body>
+{
+  constructor(public readonly header: Header, public readonly bodies: Body[]) {}
+
+  getBodyText(separator: string = "\n"): string {
+    return [this.header, ...this.bodies]
+      .map<string>((body) => body.parameters[0])
+      .join(separator);
   }
   mergedBody(): Header {
     return {
@@ -14,6 +21,9 @@ export class CombinedEventCommandGroup<
       indent: this.header.indent,
       parameters: [this.getBodyText()],
     } as Header;
+  }
+  joinCommandBodies(): [Header, ...Body[]] {
+    return joinHeaderAndBody(this);
   }
 
   normalizedCommands(): [Header] {
