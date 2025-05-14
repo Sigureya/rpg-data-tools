c9f2b0f1f710fd0f996686b1abf74b9bf9135171
Sigureya <sigureya1208@gmail.com>
2025-05-14 20:58:38 +0900

    ファイル移動に伴いexportを修正

--- src/eventCommand/commandMapper/commandGroup/class/index.ts
+++ src/eventCommand/commandMapper/commandGroup/class/index.ts
@@ -1,6 +1,5 @@
 export * from "./commandGroop";
 export * from "./types/groopTypes";
 export * from "./createGroup";
-export * from "./join";
 export * from "./commentUtils";
 export * from "./types";
