f6b0f60733086460967e0ecc87babe48c490a6ca
Sigureya <sigureya1208@gmail.com>
2025-05-16 11:46:46 +0900

    テストケースを改良。パラメータ違いをテストしやすくした

--- src/eventCommand/commandMapper/commandGroup/class/types/simple.test.ts
+++ src/eventCommand/commandMapper/commandGroup/class/types/simple.test.ts
@@ -3,12 +3,63 @@ import { SimpleEventCommandGroup } from "./simple";
 import {
   createEventCommand,
   makeCommandShowMessage,
+  makeCommandShowMessageBody,
   SHOW_MESSAGE_BODY,
 } from "@sigureya/rpgtypes";
 import type {
   Command_ShowMessage,
   Command_ShowMessageBody,
+  EventCommand,
 } from "@sigureya/rpgtypes";
+
+const testXXX = (
+  header: Command_ShowMessage,
+  xxxx: string[],
+  expectedText: string
+) => {
+  const group = new SimpleEventCommandGroup(
+    SHOW_MESSAGE_BODY,
+    header,
+    xxxx.map((text) => createEventCommand(SHOW_MESSAGE_BODY, [text]))
+  );
+  describe("", () => {
+    test("should return the correct body text", () => {
+      expect(group.getBodyText()).toEqual(expectedText);
+    });
+    const body: Command_ShowMessageBody = group.mergedBody();
+
+    test("", () => {
+      expect(body).toEqual({
+        code: SHOW_MESSAGE_BODY,
+        parameters: [expectedText],
+        indent: 0,
+      } satisfies Command_ShowMessageBody);
+    });
+    test("", () => {
+      expect(group.bodies).not.toContain(body);
+    });
+  });
+  describe("", () => {
+    const result: EventCommand[] = group.normalizedCommands();
+    test("", () => {
+      expect(result[0]).toEqual(header);
+    });
+    test("", () => {
+      expect(result[0]).not.toBe(header);
+    });
+  });
+};
+
+describe("", () => {
+  testXXX(makeCommandShowMessage({}), [], "");
+  testXXX(makeCommandShowMessage({}), ["xxx  "], "xxx");
+  testXXX(makeCommandShowMessage({}), ["aaa", "bbb", "ccc"], "aaa\nbbb\nccc");
+  testXXX(
+    makeCommandShowMessage({}),
+    [" aaa", "bbb\n", "ccc  "],
+    " aaa\nbbb\nccc"
+  );
+});
 
 describe("SimpleEventCommandGroup - Edge Cases", () => {
   test("Empty bodies array", () => {
@@ -44,18 +95,45 @@ describe("SimpleEventCommandGroup - Edge Cases", () => {
       speakerName: "Header",
     });
     const bodies: Command_ShowMessageBody[] = [
-      createEventCommand(SHOW_MESSAGE_BODY, ["Line1"]),
-      createEventCommand(SHOW_MESSAGE_BODY, ["Line2"]),
+      createEventCommand(SHOW_MESSAGE_BODY, ["aaa"]),
+      createEventCommand(SHOW_MESSAGE_BODY, ["bbb"]),
     ];
     const group = new SimpleEventCommandGroup(
       SHOW_MESSAGE_BODY,
       header,
       bodies
     );
-    expect(group.getBodyText()).toBe("Line1\nLine2"); // ボディが正しく結合される
+    expect(group.getBodyText()).toBe("aaa\nbbb"); // ボディが正しく結合される
     expect(group.normalizedCommands()).toEqual([
       header,
-      createEventCommand(SHOW_MESSAGE_BODY, ["Line1\nLine2"]),
+      createEventCommand(SHOW_MESSAGE_BODY, ["aaa\nbbb"]),
     ]);
+  });
+});
+describe("getBodyText", () => {
+  test("", () => {
+    const group = new SimpleEventCommandGroup(
+      SHOW_MESSAGE_BODY,
+      makeCommandShowMessage({}),
+      [] as Command_ShowMessageBody[]
+    );
+    expect(group.getBodyText()).toBe("");
+  });
+
+  test("", () => {
+    const group = new SimpleEventCommandGroup(
+      SHOW_MESSAGE_BODY,
+      makeCommandShowMessage({}),
+      [makeCommandShowMessageBody("aaa   ")]
+    );
+    expect(group.getBodyText()).toBe("aaa");
+  });
+  test("should return the correct body text", () => {
+    const group = new SimpleEventCommandGroup(
+      SHOW_MESSAGE_BODY,
+      makeCommandShowMessage({}),
+      [makeCommandShowMessageBody("aaa"), makeCommandShowMessageBody("bbb")]
+    );
+    expect(group.getBodyText()).toBe("aaa\nbbb");
   });
 });
