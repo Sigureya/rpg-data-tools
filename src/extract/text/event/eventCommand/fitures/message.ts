24f83ad82efc78a4fc7275074ae7caa72ebb6dbd
Sigureya <sigureya1208@gmail.com>
2025-05-17 07:39:12 +0900

    メッセージから必要な情報を抽出する。発言者情報の取得も必要なため、かなり複雑化している

---
+++ src/extract/text/event/eventCommand/fitures/message.ts
@@ -0,0 +1,14 @@
+import type { EventCommandGroup_Message } from "src/eventCommand";
+import { pickSpeakerName } from "../speaker";
+import type { TextCommandParameter } from "./types";
+
+export const extractTextParamFromMessage = (
+  messageCommand: EventCommandGroup_Message
+): Required<TextCommandParameter> => {
+  return {
+    code: messageCommand.bodyCode,
+    paramIndex: 0,
+    value: messageCommand.getBodyText(),
+    speaker: pickSpeakerName(messageCommand.header),
+  };
+};
