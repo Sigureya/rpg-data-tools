3e67eba79f2dbebb230c908f571e4611b77944a9
Sigureya <sigureya1208@gmail.com>
2025-05-16 09:42:29 +0900

    テストケースの名前のみ修正

--- src/eventCommand/commandMapper/commandGroup/class/message.test.ts
+++ src/eventCommand/commandMapper/commandGroup/class/message.test.ts
@@ -27,7 +27,19 @@ describe("makeCommmand", () => {
   });
 });
 
-describe("", () => {
+describe("createMessageGroup", () => {
+  test("should create a valid Command_ShowMessage instance", () => {
+    const command: Command_ShowMessage = makeCommandShowMessage({});
+    const expected: Command_ShowMessage = {
+      code: SHOW_MESSAGE,
+      parameters: ["", 0, 0, 2, ""],
+      indent: 0,
+    };
+    expect(command).toEqual(expected);
+  });
+});
+
+describe("createMessageGroup and extractMessageGroup", () => {
   const mockCommands: EventCommand[] = [
     makeCommand2_CommonEvent({ eventId: 6 }),
     makeCommandShowMessage({
@@ -40,19 +52,25 @@ describe("", () => {
     makeCommand2_CommonEvent({ eventId: 10 }),
   ];
   const result = createMessageGroup(mockCommands, 1);
-  describe("", () => {
-    expect(result).toBeInstanceOf(SimpleEventCommandGroup);
-    test("", () => {
+
+  describe("SimpleEventCommandGroup instance validation", () => {
+    test("should create an instance of SimpleEventCommandGroup", () => {
+      expect(result).toBeInstanceOf(SimpleEventCommandGroup);
+    });
+
+    test("should return correct body text", () => {
       expect(result.getBodyText()).toBe("aaa\nbbb\nccc");
     });
-    test("", () => {
+
+    test("should return correct merged body", () => {
       expect(result.mergedBody()).toEqual({
         code: SHOW_MESSAGE_BODY,
         indent: 0,
         parameters: ["aaa\nbbb\nccc"],
       });
     });
-    test("", () => {
+
+    test("should return normalized commands", () => {
       const newCommands: EventCommand[] = result.normalizedCommands();
       const expectedCommands: EventCommand[] = [
         makeCommandShowMessage({
@@ -64,7 +82,8 @@ describe("", () => {
       expect(newCommands).toEqual(expectedCommands);
     });
   });
-  describe("", () => {
+
+  describe("extractMessageGroup validation", () => {
     const expected: EventCommandGroupBase<
       Command_ShowMessageHeader,
       Command_ShowMessageBody
@@ -79,10 +98,12 @@ describe("", () => {
         { code: 401, parameters: ["ccc"], indent: 0 },
       ] satisfies Command_ShowMessageBody[],
     };
-    test("", () => {
+
+    test("should extract a valid message group", () => {
       expect(extractMessageGroup(mockCommands, 1)).toEqual(expected);
     });
-    test("", () => {
+
+    test("should match the extracted group with the created group", () => {
       expect(result).toMatchObject(expected);
     });
   });
